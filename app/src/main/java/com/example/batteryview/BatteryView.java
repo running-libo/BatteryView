21777b79e9bc1aa8f05ea8db113e35920b818e5a
apple <18201497620@163.com>
2020-07-22 18:14:08 +0800

    initCommit

---
+++ app/src/main/java/com/example/batteryview/BatteryView.java
@@ -0,0 +1,23 @@
+package com.example.batteryview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import androidx.annotation.Nullable;
+
+/**
+ * create by libo
+ * create on 2020/7/22
+ * description
+ */
+public class BatteryView extends View {
+
+    public BatteryView(Context context) {
+        super(context);
+    }
+
+    public BatteryView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+}
